edeb69b20e8795f07aaf60d3041012498b98192c
FilipchukB <bohdanfil2015@gmail.com>
2020-05-14 18:25:47 +0300

    test

---
+++ Diary/splanerapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Post
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
+
--- Diary/splanerapp/views.py
+++ Diary/splanerapp/views.py
@@ -1,5 +1,20 @@
+import requests
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
 
 
 def index(request):
-    return render(request, 'splanerapp/index.html')
+    post = Post.objects.all
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+
+        form = PostForm()
+
+    return render(request, 'splanerapp/index.html', {
+        'posts': post,
+        'form': form
+    })
