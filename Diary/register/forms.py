52bf164295361b6a2baa4d23a2d4f59767ae10bf
Illya-pas <illya.pasi4nik08@gmail.com>
2020-05-24 20:01:28 +0300

    revealed how to change

--- Diary/register/forms.py
+++ Diary/register/forms.py
@@ -1,19 +1,23 @@
+from typing import Any
+
 from allauth.account import forms
-from allauth.account.forms import LoginForm, PasswordField
+from allauth.account.forms import LoginForm, PasswordField, BaseSignupForm
 from allauth.account.templatetags import account
 from django.forms import TextInput, PasswordInput, BooleanField, CharField
 
 
 class YourLoginForm(LoginForm):
 
-    remember = BooleanField(label=(""),
-                                  required=False)
+    remember = None
+    # remember = BooleanField(label=(""),
+    #                         required=False)
     password = PasswordField(label=(""))
-
-
 
     def __init__(self, *args, **kwargs):
         super(YourLoginForm, self).__init__(*args, **kwargs)
         self.fields['login'].widget = TextInput(
-            attrs={'class': 'signin_form form', 'placeholder': 'Name', 'label': 'None'})
+            attrs={'class': 'signin_form form', 'placeholder': 'Name'})
         self.fields['password'].widget = PasswordInput(attrs={'class': 'signin_form form', 'placeholder': 'Password'})
+
+custom_form: Any = super(BaseSignupForm, self):
+    username.label = ''
